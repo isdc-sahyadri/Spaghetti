caf3572ff66a14c6f0a3db78c029229362d3940d
mmuzammil2003 <muzammilamir03@gmail.com>
2025-02-08 14:10:15 +0530

    u3

--- lostandfound/l_f/urls.py
+++ lostandfound/l_f/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="homepage"),
+    path("",views.home, name="home"),
+    path("find",views.find_item,name="find_item"),
+    path("post",views.post_item,name="post_item"),
 ]
